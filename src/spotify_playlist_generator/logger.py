7510b5e1dad3e82fcfd4ae9cc02d4735554a1ae6
Kaiano Levine <kaiano.levine@gmail.com.com>
2025-10-26 12:30:47 -0500

    Updating logger

--- src/spotify_playlist_generator/logger.py
+++ src/spotify_playlist_generator/logger.py
@@ -1,14 +1,32 @@
 import datetime
 import logging
+import os
+
+from dotenv import load_dotenv
+
+# Load from .env if it exists (useful for local development)
+load_dotenv()
+
 
 default_level = logging.WARNING
+level_to_set = os.getenv("LOGGING_LEVEL", "").upper()
+valid_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+if level_to_set in valid_levels:
+    logging.getLogger().warning(f"Logger level set to: {level_to_set}")
+else:
+    logging.getLogger().warning(
+        f"Invalid logging level: {level_to_set}. Level not changed.\n"
+        f"default_level: {default_level}"
+    )
+    level_to_set = default_level
+
 logging.basicConfig(
-    level=default_level,
+    level=level_to_set,
     format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d - %(funcName)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-logger = logging.getLogger("spot")
-logging.getLogger().warning(f"Logger level set to: {default_level}")
+logger = logging.getLogger("kaiano_common_utils")
+logging.getLogger().warning(f"Logger level set to: {level_to_set}")
 
 # Shortcut aliases
 debug = logger.debug
@@ -16,20 +34,6 @@ info = logger.info
 warning = logger.warning
 error = logger.error
 exception = logger.exception
-
-
-def set_logger_level(level_to_set):
-    normalized_level = level_to_set.upper()
-    valid_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-    if normalized_level in valid_levels:
-        logger.setLevel(getattr(logging, normalized_level))
-        logging.getLogger().warning(f"Logger level set to: {normalized_level}")
-    else:
-        logging.getLogger().warning(
-            f"Invalid logging level: {level_to_set}. Level not changed.\n"
-            f"default_level: {default_level}, or DEBUG"
-        )
-    return logger
 
 
 def get_logger():
